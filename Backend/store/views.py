c19120a70b0a81c9b08579fae9c09e6fbcdc96ae
Umesh Nishad <umeshnishad3001@gmail.com>
2026-09-13 20:24:13 +0530

    Day6

--- Backend/store/views.py
+++ Backend/store/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Category, Product, Cart, CartItem
+from .models import Category, Product, Cart, CartItem,Order,OrderItem
 from .serializers import CategorySerializer, ProductSerializer, CartSerializer
 
 @api_view(['GET'])
@@ -90,3 +90,56 @@ def remove_from_cart(request):
     item_id = request.data.get('item_id')
     CartItem.objects.filter(id=item_id).delete()
     return Response({'message': 'Product removed from cart'})
+
+
+@api_view(['POST'])
+def create_order(request):
+    try:
+        data = request.data
+
+        name = data.get('name')
+        address = data.get('address')
+        phone = data.get('phone')
+        payment_method = data.get('payment_method', 'COD')
+
+        cart = Cart.objects.first()
+
+        if not cart or not cart.items.exists():
+            return Response(
+                {'error': 'Cart is empty'},
+                status=400
+            )
+
+        total = sum(
+            float(item.product.price) * item.quantity
+            for item in cart.items.all()
+        )
+
+        # Create Order
+        order = Order.objects.create(
+            user=None,
+            total_price=total,
+        )
+
+        # Create Order Items
+        for item in cart.items.all():
+            OrderItem.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity,
+                price=item.product.price,
+            )
+
+        # Clear the cart
+        cart.items.all().delete()
+
+        return Response({
+            "message": "Order Placed Successfully",
+            "order_id": order.id,
+        }, status=201)
+
+    except Exception as e:
+        return Response(
+            {"error": str(e)},
+            status=500
+        )
